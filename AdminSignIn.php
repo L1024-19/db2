b38f8fd51f3fcbe255c7dd80c6b2d5fc2ec140ff
jamestanprogrammer@gmail.com <jamestanprogrammer@gmail.com>
2020-03-14 16:02:42 -0400

    login sessions now correspond to their respective titles

--- AdminSignIn.php
+++ AdminSignIn.php
@@ -4,23 +4,23 @@
 session_start();
 
 if (!empty($_POST)) {
-    if (empty($_POST['parentemail']) || empty($_POST['parentpassword'])) {
+    if (empty($_POST['adminemail']) || empty($_POST['adminpassword'])) {
         echo "Fields cannot be left blank.<br>";
     }
-    else if (isset($_POST['parentemail']) && isset($_POST['parentpassword'])) {
+    else if (isset($_POST['adminemail']) && isset($_POST['adminpassword'])) {
         // Getting submitted user data from database
         $myconnection = new mysqli('localhost', 'root', '', 'db2');
-        $stmt = $myconnection->prepare("SELECT email, password FROM parents, users WHERE email = ? ".
-        "AND parents.parent_id = users.id");
-        $stmt->bind_param('s', $_POST['parentemail']);
+        $stmt = $myconnection->prepare("SELECT email, password FROM admins, users WHERE email = ? ".
+        "AND admins.admin_id = users.id");
+        $stmt->bind_param('s', $_POST['adminemail']);
         $stmt->execute();
         $result = $stmt->get_result();
     	$user = $result->fetch_object();
 
     	// Verify user password and set $_SESSION
-    	if ($user != NULL && password_verify($_POST['parentpassword'], $user->password)) {
+    	if ($user != NULL && $_POST['adminpassword'] == $user->password) {
             $_SESSION['user_id'] = $user->id;
-            header('Location: ParentDashboard.php');
+            header('Location: AdminDashboard.php');
         }
         else {
             echo("Either Email or Password is incorrect.");
--- StudentSignIn.php
+++ StudentSignIn.php
@@ -4,22 +4,26 @@
 session_start();
 
 if (!empty($_POST)) {
-    if (isset($_POST['studentemail']) && isset($_POST['studentpassword'])) {
+    if (empty($_POST['studentemail']) || empty($_POST['studentpassword'])) {
+        echo "Fields cannot be left blank.<br>";
+    }
+    else if (isset($_POST['studentemail']) && isset($_POST['studentpassword'])) {
         // Getting submitted user data from database
         $myconnection = new mysqli('localhost', 'root', '', 'db2');
-        $stmt = $myconnection->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt = $myconnection->prepare("SELECT email, password FROM students, users WHERE email = ? ".
+        "AND students.student_id = users.id");
         $stmt->bind_param('s', $_POST['studentemail']);
         $stmt->execute();
         $result = $stmt->get_result();
     	$user = $result->fetch_object();
 
-    	// Verify user password and set $_SESSION
-    	if (password_verify($_POST['studentpassword'], $user->password)) {
+        // Verify user password and set $_SESSION
+    	if ($user != NULL && password_verify($_POST['studentpassword'], $user->password)) {
             $_SESSION['user_id'] = $user->id;
             header('Location: StudentSetting.html');
         }
         else {
-            echo("Something went wrong");
+            echo("Either Email or Password is incorrect.");
         }
     }
 }
